32318c8ba2bf6d6d241f0b86c2465fd0f78f6b3b
Jerome Wy <jeromedaniel.wy@gmail.com>
2022-02-16 18:00:01 -0800

    Deleted unncessary files. Boilerplate completed for components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {Router, Route, Switch} from 'react-router-dom'
+import Header from './components/Header'
+import About from './components/About'
+import Experience from './components/Experience'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
 
 function App() {
 	return (
 		<div className='App'>
+			
+			<nav>
+				<Header />
+			</nav>
+<Switch>
+	<Route exact path='/' component={Home} />
+	<Route exact path='/about' component={About} />
+	<Route exact path='/experience' component={Experience} />
+	<Route exact path='/projects' component={Projects} />
+	<Route exact path='/skills' component={Skills} />
+	
+</Switch>
+
 			<h1>Coming soon</h1>
 		</div>
 	);
