1c416a36ddd9c012ff479e786e374612b2c01ab0
Jerome Wy <jeromedaniel.wy@gmail.com>
2022-02-23 11:14:25 -0800

    Fixed and added content in Projects

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -3,70 +3,137 @@ import React from 'react';
 export default function Projects() {
 	return (
 		<div className='Projects'>
-			<h1>// project name</h1>
-			<h1>// description</h1>
+			<div className='project-item-1-container'>
+				<div className='project-item-1'>
+					<img
+						src='https://camo.githubusercontent.com/7b35060a783d9d39a3dc7d9f66a3c31db44b7f7eb94dccfe909cc6591d6f985f/68747470733a2f2f692e696d6775722e636f6d2f4f6a72625633432e706e67'
+						alt='screenshot-1'
+					/>
+					<img
+						src='https://camo.githubusercontent.com/7b35060a783d9d39a3dc7d9f66a3c31db44b7f7eb94dccfe909cc6591d6f985f/68747470733a2f2f692e696d6775722e636f6d2f4f6a72625633432e706e67'
+						alt='screenshot-1'
+					/>
 
-			<div className='project-item-1'>
-				<h1>NerdFeed</h1>
-				<h2>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni numquam
-					est neque ducimus sint, saepe voluptates! Tempora dolore odit nulla
-					quibusdam illo vel deleniti non id est? Ipsa, cumque eos?
-				</h2>
+					<img
+						src='https://camo.githubusercontent.com/7b35060a783d9d39a3dc7d9f66a3c31db44b7f7eb94dccfe909cc6591d6f985f/68747470733a2f2f692e696d6775722e636f6d2f4f6a72625633432e706e67'
+						alt='screenshot-1'
+					/>
+
+					<img
+						src='https://camo.githubusercontent.com/7b35060a783d9d39a3dc7d9f66a3c31db44b7f7eb94dccfe909cc6591d6f985f/68747470733a2f2f692e696d6775722e636f6d2f4f6a72625633432e706e67'
+						alt='screenshot-1'
+					/>
+				</div>
+
+				<div className='project-item-1-desc'>
+					<h1>NerdFeed</h1>
+					<p>
+						This project was created as my second Project for General Assembly's
+						Software Engineering Immersive Porogram
+					</p>
+					<ul>
+						<li>
+							Created a web application using MongoDB, Express, React, and Node
+						</li>
+						<li>
+							Full CRUD Functionality to create, read, update, and delete posts
+						</li>
+						<li>Posts are stored on an external database: MongoDB Compass</li>
+					</ul>
+				</div>
 			</div>
 
-			<div className='project-item-1'>
-				Created a web application using MongoDB, Express, React, and Node Full
-				CRUD Functionality to create, read, update, and delete posts Posts are
-				stored on an external database: MongoDB Compass
+			<div className='project-item-2-container'>
+				<div className='project-item-2-desc'>
+					<h1>Anchr</h1>
+					Full Stack app created using PostgreSQL, Express, React, and Node Task
+					prioritization application created to better manage time and
+					prioritize specific tasks User authentication implemented for both
+					Front-End and Back-End using JWT Summarized chart provided using React
+					Library - Recharts App successfully deployed on Heroku
+				</div>
+
+				<div className='project-item-2'>
+					<img
+						src='https://camo.githubusercontent.com/9bff2d17f4d8be10c0d60513d30f240441f2d741724b6c052d0384229d88a138/68747470733a2f2f692e696d6775722e636f6d2f4b6c596d666e532e706e67'
+						alt='screenshot-1'
+					/>
+					<img
+						src='https://camo.githubusercontent.com/ddb82c9cf658f490e1ec529adbd4976e10f1f0b816e961507a58ee3c2b59220e/68747470733a2f2f692e696d6775722e636f6d2f514b74644454492e706e67'
+						alt='screenshot-2'
+					/>
+
+					<img
+						src='https://camo.githubusercontent.com/93af335a615b16fc4e8a4bb8fa92ea750b865afea3daa4eaefcbe467688ec67c/68747470733a2f2f692e696d6775722e636f6d2f704d4a5976676f2e706e67'
+						alt='screenshot-3'
+					/>
+
+					<img
+						src='https://camo.githubusercontent.com/7b35060a783d9d39a3dc7d9f66a3c31db44b7f7eb94dccfe909cc6591d6f985f/68747470733a2f2f692e696d6775722e636f6d2f4f6a72625633432e706e67'
+						alt='screenshot-1'
+					/>
+				</div>
 			</div>
 
-			<div className='project-item-2'>
-				<h1>Anchr</h1>
-				<h2>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni numquam
-					est neque ducimus sint, saepe voluptates! Tempora dolore odit nulla
-					quibusdam illo vel deleniti non id est? Ipsa, cumque eos?
-				</h2>
+			<div className='project-item-3-container'>
+				<div className='project-item-3'>
+					<img
+						src='https://camo.githubusercontent.com/9bff2d17f4d8be10c0d60513d30f240441f2d741724b6c052d0384229d88a138/68747470733a2f2f692e696d6775722e636f6d2f4b6c596d666e532e706e67'
+						alt='screenshot-1'
+					/>
+					<img
+						src='https://camo.githubusercontent.com/ddb82c9cf658f490e1ec529adbd4976e10f1f0b816e961507a58ee3c2b59220e/68747470733a2f2f692e696d6775722e636f6d2f514b74644454492e706e67'
+						alt='screenshot-2'
+					/>
+
+					<img
+						src='https://camo.githubusercontent.com/93af335a615b16fc4e8a4bb8fa92ea750b865afea3daa4eaefcbe467688ec67c/68747470733a2f2f692e696d6775722e636f6d2f704d4a5976676f2e706e67'
+						alt='screenshot-3'
+					/>
+
+					<img
+						src='https://camo.githubusercontent.com/7b35060a783d9d39a3dc7d9f66a3c31db44b7f7eb94dccfe909cc6591d6f985f/68747470733a2f2f692e696d6775722e636f6d2f4f6a72625633432e706e67'
+						alt='screenshot-1'
+					/>
+				</div>
+
+				<div className='project-item-3-desc'>
+					<h1>Calendr</h1>
+					Full Stack app created using Python + Django, Vue, and Node Utilized
+					VueLibrary V-Calendar to generate calendar User is able to select a
+					specific date and add a task or event App created in only 36 hours
+				</div>
 			</div>
 
-			<div className='project-item-2'>
-				Full Stack app created using PostgreSQL, Express, React, and Node Task
-				prioritization application created to better manage time and prioritize
-				specific tasks User authentication implemented for both Front-End and
-				Back-End using JWT Summarized chart provided using React Library -
-				Recharts App successfully deployed on Heroku
-			</div>
+			<div className='project-item-4-container'>
+				<div className='project-item-4'>
+					<h1>Procrastigamr</h1>
+					Full stack app created using Python + Django, React, and Node Python
+					Django + Django Rest Framework used for back-end CRUD Users are able
+					to add a game to a local database as well as add reviews Used CSS Grid
+					and Flexbox for front-end styling and organization
+				</div>
 
-			<div className='projects-item-3'>
-				<h1>Calendr</h1>
-				<h2>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni numquam
-					est neque ducimus sint, saepe voluptates! Tempora dolore odit nulla
-					quibusdam illo vel deleniti non id est? Ipsa, cumque eos?
-				</h2>
-			</div>
+				<div className='project-item-4'>
+					<img
+						src='https://camo.githubusercontent.com/9bff2d17f4d8be10c0d60513d30f240441f2d741724b6c052d0384229d88a138/68747470733a2f2f692e696d6775722e636f6d2f4b6c596d666e532e706e67'
+						alt='screenshot-1'
+					/>
+					<img
+						src='https://camo.githubusercontent.com/ddb82c9cf658f490e1ec529adbd4976e10f1f0b816e961507a58ee3c2b59220e/68747470733a2f2f692e696d6775722e636f6d2f514b74644454492e706e67'
+						alt='screenshot-2'
+					/>
 
-			<div className='projects-item-3'>
-				Full Stack app created using Python + Django, Vue, and Node Utilized
-				VueLibrary V-Calendar to generate calendar User is able to select a
-				specific date and add a task or event App created in only 36 hours
-			</div>
+					<img
+						src='https://camo.githubusercontent.com/93af335a615b16fc4e8a4bb8fa92ea750b865afea3daa4eaefcbe467688ec67c/68747470733a2f2f692e696d6775722e636f6d2f704d4a5976676f2e706e67'
+						alt='screenshot-3'
+					/>
 
-			<div className='projects-item-4'>
-				<h1>Procrastigamr</h1>
-				<h2>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni numquam
-					est neque ducimus sint, saepe voluptates! Tempora dolore odit nulla
-					quibusdam illo vel deleniti non id est? Ipsa, cumque eos?
-				</h2>
-			</div>
-
-			<div className='projects-item-4'>
-				Full stack app created using Python + Django, React, and Node Python
-				Django + Django Rest Framework used for back-end CRUD Users are able to
-				add a game to a local database as well as add reviews Used CSS Grid and
-				Flexbox for front-end styling and organization
+					<img
+						src='https://camo.githubusercontent.com/7b35060a783d9d39a3dc7d9f66a3c31db44b7f7eb94dccfe909cc6591d6f985f/68747470733a2f2f692e696d6775722e636f6d2f4f6a72625633432e706e67'
+						alt='screenshot-1'
+					/>
+				</div>
 			</div>
 		</div>
 	);
